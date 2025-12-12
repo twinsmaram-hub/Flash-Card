0f5f8830d00483356f0b6c4bba5c3f7fc28635ab
twinsmaram-hub <twinsmaram@gmail.com>
2025-12-13 02:18:25 +0300

    Create and show

--- controllers/cards.js
+++ controllers/cards.js
@@ -24,15 +24,43 @@ router.get('/new', async (req, res) => {
     res.redirect('/');
   }
 });
-
 router.post('/', async (req, res) => {
   try {
     req.body.owner = req.session.user._id;
-    await Listing.create(req.body);
+    await Cards.create(req.body);
     res.redirect('/cards');
   } catch (error) {
     console.error(error);
     res.redirect('/cards/new');
+  }
+});
+
+
+// Show
+router.get('/:id', async (req, res) => {
+  try {
+    const card = await Cards.findById(req.params.id).populate('owner');
+    res.render('cards/show.ejs', { card });
+  } catch (error) {
+    console.error(error);
+    res.redirect('/cards');
+  }
+});
+// Delete
+router.delete('/:id', async (req, res) => {
+  try {
+    const card = await Cards.findById(req.params.id);
+    const isOwner = card.owner.equals(req.session.user._id);
+
+    if (isOwner) {
+      await card.deleteOne();
+      res.redirect('/cards');
+    } else {
+      throw new Error(`Permission Denied to ${req.session.user.username}`);
+    }
+  } catch (error) {
+    console.error(error);
+    res.redirect('/cards');
   }
 });
 
